2f9b8a2d01ac91973c9f0fb9552d25fe6012ec80
刘建阳 <liujianyang@lattebank.com>
2018-04-25 17:16:05 +0800

    init

---
+++ credittools-common/src/main/java/cn/caijiajia/credittools/common/constant/CredittoolsConstants.java
@@ -0,0 +1,9 @@
+package cn.caijiajia.credittools.common.constant;
+
+/**
+ * Created by liujianyang on 18/4/25.
+ */
+public class CredittoolsConstants {
+
+
+}
---
+++ credittools/src/main/java/cn/caijiajia/credittools/controller/LocalTestController.java
@@ -0,0 +1,26 @@
+/**
+ * Caijiajia confidential
+ * <p>
+ * Copyright (C) 2017 Shanghai Shuhe Co., Ltd. All rights reserved.
+ * <p>
+ * No parts of this file may be reproduced or transmitted in any form or by any means,
+ * electronic, mechanical, photocopying, recording, or otherwise, without prior written
+ * permission of Shanghai Shuhe Co., Ltd.
+ */
+package cn.caijiajia.credittools.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liujianyang on 2018/4/25.
+ */
+@RestController
+public class LocalTestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public void test(){
+        System.out.println("进入test");
+    }
+}
