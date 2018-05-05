9091ffa3651602ea513ab7e18e575e935bf2be9f
陈冬 <chendongdong@lattebank.com>
2018-05-06 00:40:33 +0800

    refine

--- credittools/src/main/java/cn/caijiajia/credittools/controller/LocalTestController.java
+++ credittools/src/main/java/cn/caijiajia/credittools/controller/LocalTestController.java
@@ -48,43 +48,7 @@ public class LocalTestController {
 
 
     public static void main(String[] args) {
-        String str = "000001";
-        int lastZero = str.lastIndexOf("0");
-        System.out.println(lastZero);
-
-        Integer rank = Integer.parseInt(str.substring(str.lastIndexOf("0") + 1,str.length())) + 99;
-        String newStr = str.substring(0, str.length() - String.valueOf(rank).length()) + String.valueOf(rank);
-        System.out.println(newStr);
-
-        String str2 = "00001400020";
-        Integer b = Integer.valueOf(str2.replaceAll("^(0+)", "")) + 123;
-        String newStr2 = str.substring(0, str2.length() - String.valueOf(b).length()) + String.valueOf(b);
-        System.out.println(newStr2);
 
     }
 
-    private static void demo1(){
-        Date date1 = new Date(Long.valueOf("1524828100946"));
-        System.out.println(date1);
-        System.out.println(String.valueOf(new Date().getTime() /1000));
-        List<String> list = new ArrayList<>();
-        list.add("nihjao");
-        list.add("好我");
-        list.add("cde");
-        String[] strArray = list.toString().split(",");
-        String listToStr = StringUtils.join(list.toArray(), ",");
-        Object[] objList =  list.toArray();
-        System.out.println(strArray);
-        System.out.println(listToStr);
-        List<String> newList = new ArrayList<>();
-        for(String listStr : strArray){
-            newList.add(listStr);
-        }
-        System.out.println(newList.toString());
-        String str = "";
-        for(Object stri : objList){
-            str = str + stri;
-        }
-        System.out.println(str);
-    }
 }
