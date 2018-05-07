c9d6b726731337d567855536a8bbbbecbebf36ac
刘建阳 <liujianyang@lattebank.com>
2018-05-07 16:40:15 +0800

    refine

---
+++ credittools/src/main/java/cn/caijiajia/credittools/bo/UnionLoginBo.java
@@ -0,0 +1,16 @@
+/**
+ * Caijiajia confidential
+ * <p>
+ * Copyright (C) 2017 Shanghai Shuhe Co., Ltd. All rights reserved.
+ * <p>
+ * No parts of this file may be reproduced or transmitted in any form or by any means,
+ * electronic, mechanical, photocopying, recording, or otherwise, without prior written
+ * permission of Shanghai Shuhe Co., Ltd.
+ */
+package cn.caijiajia.credittools.bo;
+
+/**
+ * Created by liujianyang on 2018/5/7.
+ */
+public class UnionLoginBo {
+}
