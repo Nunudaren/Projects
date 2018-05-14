5e0252afa866557ef0b9979363584b6d32d23170
chendong <chendongdong@lattebank.com>
2018-05-14 17:54:03 +0800

    fix conflict

--- credittools/src/main/java/cn/caijiajia/credittools/delegator/UserDelegator.java
+++ credittools/src/main/java/cn/caijiajia/credittools/delegator/UserDelegator.java
@@ -6,37 +6,13 @@ import cn.caijiajia.framework.exceptions.CjjClientException;
 import cn.caijiajia.framework.httpclient.HttpClientTemplate;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * @Author:chendongdong
- * @Date:2018/5/10
-
-import cn.caijiajia.framework.httpclient.HttpClientTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * @author zhouyangbo
- * @description
- * @Date：Created in 16:43 2018/5/10
-=======
 import org.springframework.stereotype.Component;
 
 /**
  * @Author:chendongdong
  * @Date:2018/5/10
->>>>>>> pengyuan login
  */
+
 @Component
 public class UserDelegator {
 
@@ -45,27 +21,14 @@ public class UserDelegator {
     @Autowired
     private HttpClientTemplate httpClient;
     @Autowired
-<<<<<<< HEAD
-=======
-    private String userUrl;
-    @Autowired
->>>>>>> pengyuan login
     private String userLoanUrl;
 
     public UserInfo getUser(String uid) {
         UserInfo userInfo = JSONObject.parseObject(httpClient.doGet(userLoanUrl + String.format(URL_USER_INFO, uid)), UserInfo.class);
-<<<<<<< HEAD
         if (userInfo == null) {
             throw new CjjClientException(ErrorResponseConstants.USER_NOT_EXISTS_CODE, ErrorResponseConstants.USER_NOT_EXISTS_MESSAGE);
         }
         return userInfo;
     }
 
-=======
-        if(userInfo == null) {
-            throw new CjjClientException(ErrorResponseConstants.USER_NOT_EXISTS_CODE,ErrorResponseConstants.USER_NOT_EXISTS_MESSAGE);
-        }
-        return userInfo;
-    }
->>>>>>> pengyuan login
 }
